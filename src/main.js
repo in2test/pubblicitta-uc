953cf2f3bae2bdea7707425c6b0938c4ee682704
in2test <116454815+in2test@users.noreply.github.com>
2026-01-12 07:52:31 +0100

    System triggers for theme

--- src/main.js
+++ src/main.js
@@ -1,24 +1,39 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const systemTrigger = document.getElementById("SystemSwitch");
+const darkTrigger = document.getElementById("DarkSwitch");
+const lightTrigger = document.getElementById("LightSwitch");
 
-setupCounter(document.querySelector('#counter'))
+systemTrigger.addEventListener("click", function () {
+	localStorage.removeItem("theme");
+	updateTheme(this);
+});
+darkTrigger.addEventListener("click", function () {
+	localStorage.theme = "dark";
+	updateTheme(this);
+});
+lightTrigger.addEventListener("click", function () {
+	localStorage.theme = "light";
+	updateTheme(this);
+});
+function updateTheme(trigger) {
+	document.documentElement.classList.toggle(
+		"dark",
+		localStorage.theme === "dark" ||
+			(!("theme" in localStorage) &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches)
+	);
+	systemTrigger.classList.remove("border-b");
+	darkTrigger.classList.remove("border-b");
+	lightTrigger.classList.remove("border-b");
+	trigger.classList.toggle("border-b");
+}
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (typeof localStorage.theme !== "undefined" && localStorage.theme !== null) {
+	if (localStorage.theme === "dark") {
+		updateTheme(darkTrigger);
+	}
+	if (localStorage.theme === "light") {
+		updateTheme(lightTrigger);
+	}
+} else updateTheme(systemTrigger);
